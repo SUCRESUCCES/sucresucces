047f598703cec6ad3b811817bee28b52d43d8b7e
밍 <sucresucces@icloud.com>
2025-03-06 12:41:13 +0900

    250306 공공데이터 api(날씨)

--- 1_SpringBoot/src/main/java/kh/springboot/board/controller/BoardController.java
+++ 1_SpringBoot/src/main/java/kh/springboot/board/controller/BoardController.java
@@ -1,11 +1,7 @@
 package kh.springboot.board.controller;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonIOException;
-
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import kh.springboot.board.exception.BoardException;
 import kh.springboot.board.model.mapper.BoardMapper;
